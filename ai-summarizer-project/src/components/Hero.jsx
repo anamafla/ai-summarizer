86aaaae5b4121e816daedeb9b75fa4cc55d5bc1e
Ana Mafla <anamafla@hotmail.com>
2023-05-05 11:42:45 -0500

    Hero section

--- ai-summarizer-project/src/components/Hero.jsx
+++ ai-summarizer-project/src/components/Hero.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import { logo } from '../assets'
+
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <header className='w-full flex justify-center items-center flex-col'>
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={logo} alt="" className="w-28 object-contain" />
+        <button type="button" onClick={()=> window.open('https://github.com/anamafla/ai-summarizer')} className='black_btn'>Github</button>
+      </nav>
+      <h1 className="head_text">
+        Summarize Articles with <br className='max-md:hidden'/>
+        <span className='orange_gradient'>OpenAI GPT-4</span>
+      </h1>
+      <h2 className="desc">
+        Simplicy your reading with Summize, an open-source article summaarizer that transforms lengthy articles into clear and concise summaries
+      </h2>
+    </header>
   )
 }
 
